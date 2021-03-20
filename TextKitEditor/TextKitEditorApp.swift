b6f06945f820ead00bc1eba6637dbd4491389c98
Mattia Righetti <matt95.righetti@gmail.com>
2021-03-20 15:13:28 +0100

    Initial Commit

---
+++ TextKitEditor/TextKitEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextKitEditorApp.swift
+//  TextKitEditor
+//
+//  Created by Mattia Righetti on 20/03/21.
+//
+
+import SwiftUI
+
+@main
+struct TextKitEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
